7cb33c7bf6ed75384c52fd800977d72ebdc1a784
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-10 11:17:29 +0530

    Added: Calculate Daily Wage

--- src/com/bridgelabz/EmployeeWage/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage/EmployeeWage.java
@@ -10,18 +10,21 @@ public class EmployeeWage {
 		
 		int wagePerHrs = 20;
 		int fullDayHrs = 8;
+		int halfDayHrs = 4;
 		int salary = 0;
-		int randomNum = random.nextInt(2);
+		int randomNum = random.nextInt(3);
 
 		if (randomNum == 1) {
 			System.out.println("Employee is Present");
 			salary = wagePerHrs * fullDayHrs;
+		}else if (randomNum == 2) {
+			salary = wagePerHrs * halfDayHrs;
+			System.out.println("Employee is Present for half day");
 		}
 		else
 			System.out.println("Employee is Absent");
 		
 		System.out.println("Salary is: " + salary);
 	}
-
 
 }
