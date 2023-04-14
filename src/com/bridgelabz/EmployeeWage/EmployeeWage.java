09814b24d3c84b6bac807efcdc0baab04e277d34
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-14 14:15:16 +0530

    Added : Calculate wages till a condition of total working hours or day is reached

--- src/com/bridgelabz/EmployeeWage/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage/EmployeeWage.java
@@ -13,31 +13,33 @@ public class EmployeeWage {
 		int halfDayHrs = 4;
 		int salary = 0;
 		int monthlysalary = 0;
-		int workingDaysPerMonth = 20;
+		int days = 0;
+		int monthlyHrs = 0;
 		
-		for (int i = 1; i <= workingDaysPerMonth; i++) {
+		while (days != 20 && monthlyHrs != 100) {
+			days++;
 		int randomNum = random.nextInt(3);
 		
 		switch (randomNum) {
 
 		case 0:
-			System.out.println("Employee is Absent");
-			System.out.println("Monthly Salary will be: ");
+			//System.out.println("Employee is Absent");
+			//System.out.println("Monthly Salary will be: ");
 			break;
 		case 1:
-			System.out.println("Employee is Present");
+			//System.out.println("Employee is Present");
 			System.out.println("Monthly salary for Full Day: ");
 			salary = wagePerHrs * fullDayHrs;
 			break;
 		case 2:
-			System.out.println("Employee is Present");
+			//System.out.println("Employee is Present");
 			System.out.println("Monthly Salary for Half Day: ");
 			salary = wagePerHrs * halfDayHrs;
 			break;
 
 		}
 		monthlysalary = monthlysalary + salary;
-		System.out.println(+salary);
+		System.out.println("Day" + days + "Salary is: " + salary);
 
 	}
 	System.out.println("Monthly Salary: " + monthlysalary);
