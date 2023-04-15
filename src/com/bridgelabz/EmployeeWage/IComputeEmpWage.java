f5c38cfd0024a70d6b5483941a256375972ec62f
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-15 18:33:57 +0530

    Refactor: Manage employee wage of multiple companies using interface

---
+++ src/com/bridgelabz/EmployeeWage/IComputeEmpWage.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.EmployeeWage;
+
+public interface IComputeEmpWage {
+	
+	   public void addCompany(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth);
+	   public void computeEmpWage();
+
+}
